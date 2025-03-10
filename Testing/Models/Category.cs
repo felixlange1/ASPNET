742fbf032007f2bf9392f342e02a83bafbe9ecc4
Felix Lange <felixlange1@gmail.com>
2025-03-10 00:31:09 -0700

    added create product

---
+++ Testing/Models/Category.cs
@@ -0,0 +1,9 @@
+using System;
+namespace Testing.Models;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string Name { get; set; }
+
+}
--- Testing/ProductRepository.cs
+++ Testing/ProductRepository.cs
@@ -28,5 +28,23 @@ public class ProductRepository : IProductRepository
     {
         _connection.Execute($"UPDATE Products SET Name = @Name, Price = @Price WHERE ProductID = @id", new { name = product.Name, price = product.Price, id = product.ProductID });
     }
+
+    public Product AssignCategory()
+    {
+        var categoryList = GetCategories();
+        var product = new Product();
+        product.Categories = categoryList;
+        return product;
+    }
+
+    public IEnumerable<Category> GetCategories()
+    {
+        return _connection.Query<Category>("SELECT * FROM Categories");
+    }
     
+    public void InsertProduct(Product productToInsert)
+    {
+        _connection.Execute("INSERT INTO products (NAME, PRICE, CATEGORYID) VALUES (@name, @price, @categoryID);",
+            new {name = productToInsert.Name, price = productToInsert.Price, categoryID = productToInsert.CategoryID });
+    }
 }
